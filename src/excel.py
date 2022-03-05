edc245d20a24d009891194117522c0ec60b0ed4f
Marmik Patel <marmikpatel250@gmail.com>
2022-03-05 11:54:31 +0530

    recreated excel

--- src/excel.py
+++ src/excel.py
@@ -28,7 +28,7 @@ class GetExcel:
             self.topics_sheet.cell(row=i, column=max_subtopic_length).value = '=INDEX($B1:$' + col_letter2 + str(tl)\
                                                                               + ';MATCH(\'Main\'!$A' + str(i+1) + \
                                                                               ',$A1:$A' + str(tl) + ',0))'
-        self.topics_sheet.sheet_state = 'hidden'
+        self.topics_sheet.sheet_state = 'veryHidden'
         self.topics_sheet[get_column_letter(max_subtopic_length*2) + '1'] = ' '
 
     def create(self):
